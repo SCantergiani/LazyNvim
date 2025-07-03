4fc5d5178634caf8b8d3ae8099ae836143e71fe8
SCantergiani <sebastiancantergiani@gmail.com>
2025-07-03 13:48:52 -0400

    added cursor like plugin support and disabled lint for .env files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.keymap.set
-keymap("n", "<leader>r", function()
+local set = vim.keymap.set
+
+set("n", "<leader>r", function()
   local filetype = vim.bo.filetype
   local file = vim.fn.expand("%:p")
   local cmd
@@ -16,3 +17,5 @@ keymap("n", "<leader>r", function()
   Snacks.terminal(cmd, { cwd = LazyVim.root(), auto_close = false, interactive = false })
   print(LazyVim.root())
 end, { desc = "Run current file in terminal" })
+
+set({ "n", "v" }, "gcC", "<Cmd>CBlcbox10<CR>", { desc = "Comment box", noremap = true, silent = true })
