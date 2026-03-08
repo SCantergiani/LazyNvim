5d6b39fbef180462795ee303aa75805a517f72e3
sc <sebastiancantergiani@gmail.com>
2026-03-08 15:08:56 -0300

    fuera la depresion de tener linter en .md

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,11 @@
 return {
   { "nvim-mini/mini.pairs", enabled = false },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
 }
